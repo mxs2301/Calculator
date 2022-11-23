a43a08f2bea484cc9dbe4fc22ae2388c2b6ffd49
mxs2301 <max.schopf@mailfence.com>
2022-11-23 16:20:33 +0100

    Update

--- rechner_rust/src/main.rs
+++ rechner_rust/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, world!");
+    let num = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Nope");
+
+
+        let input: u32 = input.trim().parse().expect("Please type a number!");
+
+        match input.cmp(&num){
+            Ordering::Less => println!("Zu klein"),
+            Ordering::Greater => println!("Zu groß"),
+            Ordering::Equal => {println!("Genau richtig"); break;}
+        }
+    }
+
 }
